3e26dcad0c3514eea346415cba63f29e55d244d7
Daniel Abanto <daniel4.ah@gmail.com>
2021-02-05 16:01:08 -0500

    Updates

---
+++ precache-manifest.c65665f662eab453aa458eb4fc3e6af7.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7b128a98d52190cfada",
+    "url": "/recetas_pwa/static/js/main.e7b128a9.js"
+  },
+  {
+    "revision": "e7b128a98d52190cfada",
+    "url": "/recetas_pwa/static/css/main.91d8507b.css"
+  },
+  {
+    "revision": "a380f30e78719a888b56fc907eb31fd8",
+    "url": "/recetas_pwa/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+importScripts("/recetas_pwa/precache-manifest.c65665f662eab453aa458eb4fc3e6af7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+
+ self.__precacheManifest = [].concat(self.__precacheManifest || [])
+ workbox.precaching.suppressWarnings()
+ workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+ workbox.routing.registerNavigationRoute('index.html')
+//  No importa en que parte dela ruta esta, que funcione
+
+workbox.googleAnalytics.initialize()
+//Inicializando Google Analitics para offline
+
+ workbox.routing.registerRoute(/^https?.*/,
+  workbox.strategies.networkFirst(), 'GET')
+
+// workbox.routing.registerRoute(/^https:\/\/www.themealdb.com\/api\/.*/,
+//   workbox.strategies.stateWhileRevalidate(), 'GET')
+// Esta estrategia es buena cuando no se require la mas actualizado al 
+// instante, por ejemplo optimo para recetas, 
+// pide al cache y al network a la vez
+
+//  workbox.routing.registerRoute(/^https:\/\/fonts.(?:googleapis|gstatic).com\/(.*)/), workbox.strategies.cacheFirst({
+//   cacheName: 'google-fonts-cache',
+//   plugins: [
+//     new workbox.expiration.Plugin({
+//       maxAgeSeconds: 24*60*60
+//     })
+//   ]
+// }), 'GET')
+// Cache primero, ideal para fotos o fuentes
+// Vencimiento del cache, 1 dia segun configuracion
+
+
